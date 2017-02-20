44637508a18f21aab2cddc6a570c9493c6370983
Blear <723712241@qq.com>
2017-02-20 15:49:43 +0800

    增加中文语言包

---
+++ resources/lang/zh_cn/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => '密码必须至少为六个字符并与确认密码相同.',
+    'reset' => '您的密码已重置!',
+    'sent' => '密码重置邮件已发送到你的邮箱!',
+    'token' => '密码重置连接已失效.',
+    'user' => "用户不存在.",
+
+];
